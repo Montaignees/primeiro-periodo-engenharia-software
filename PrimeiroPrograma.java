880ba93907265ad83005a5b1aa994bd349369dfc
Lucas de Montaigne Teixeira Brito <106482902+Montaignees@users.noreply.github.com>
2026-02-28 13:33:15 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprograma;
+
+/**
+ *
+ * @author Lucas de Montaigne Teixeira Brito
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        
+        
+        int ano_Nascimento = 2008;
+        int ano_Atual = 2026;
+        int idade = ano_Atual - ano_Nascimento;
+        
+        System.out.println("Lucas de Montaigne - " + idade + " anos");
+        System.out.println("Qual e o seu nome?");
+        
+    }   
+}
