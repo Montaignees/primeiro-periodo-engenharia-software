825ca23b6d1b012d9a4f9ea1583c2772cfab6043
Lucas de Montaigne Teixeira Brito <106482902+Montaignees@users.noreply.github.com>
2026-03-07 13:56:19 -0300

    Update and rename calculadora.java to tabuada.java

--- Projetos-Java/Scanner/tabuada.java
+++ Projetos-Java/Scanner/tabuada.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calculadora {
+public class tabuada {
 
     public static void main(String[] args) {
 
@@ -19,3 +19,4 @@ public class calculadora {
         entrada.close();
     }
 }
+
