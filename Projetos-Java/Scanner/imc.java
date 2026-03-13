fb39e9873ccd74bd31024fbf370819491b9ae932
Lucas de Montaigne Teixeira Brito <106482902+Montaignees@users.noreply.github.com>
2026-03-13 20:13:23 -0300

    Refactor IMC calculation and update output messages

--- Projetos-Java/Scanner/imc.java
+++ Projetos-Java/Scanner/imc.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
-
 public class imc {
-            public static void main(String[] args) {
+    public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
-        
-        System.out.println("Qual é a sua altura?");
+
+        System.out.print("Digite seu peso");
+        double peso = entrada.nextDouble();
+
+        System.out.print("digite sua altura");
         double altura = entrada.nextDouble();
 
-        System.out.println("Qual é é seu peso?");
-        double peso = entrada.nextDouble();    
-        
         double imc = peso / (altura * altura);
 
-        if (imc >= 25) {
-            System.out.println("Acima do peso");
-        } else {
+        if (imc <18.5) {
+            System.out.println("abaixo do peso");
+        } else if (imc <24.9) {
             System.out.println("Peso normal");
-    }
-    entrada.close();
-}
+        } else if (imc <29.9) {
+            System.out.println("Sobrepeso");
+        } else {
+            System.out.println("Obesidade");
+        }
+        entrada.close();
+    }       
 }
